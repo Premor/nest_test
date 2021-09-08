344195dbe78bd75d0e1bac55f3e594562860d739
Premor <spamerkik@gmail.com>
2021-09-08 20:04:17 +0300

    encapsulate cats in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController, Test1Controller } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { Test2Module } from './test2/test2.module';
-import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [Test2Module],
-  controllers: [AppController,Test1Controller, CatsController],
-  providers: [AppService, CatsService],
+  imports: [Test2Module, CatsModule],
+  controllers: [AppController,Test1Controller,],
+  providers: [AppService,],
 })
 export class AppModule {}
